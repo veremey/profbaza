e18865f9624e54b04c3a09cf5985e22da96078ac
Veremey <veremey4uk@gmail.com>
2016-01-27 23:47:53 +0200

    strt

--- build/js/common.js
+++ build/js/common.js
@@ -73,7 +73,6 @@ $(document).ready(function() {
 	});
 
 
-
 	/* Скрипт для рейтинга */
 
 	$('.stars').each(function(){
@@ -99,6 +98,44 @@ $(document).ready(function() {
 				}
 			);
 		});
+	});
+
+
+	// check select
+
+	function select() {
+		$(".js-select").each(function(){
+			var select_list = $(this).parent().find(".js-select-list");
+			var text = select_list.find("li").first().text();
+			$(this).find(".js-select-text").text(text);
+			$(this).click(function(event){
+				if ($(this).hasClass("is-active")) {
+				    $(this).removeClass("is-active");
+				    select_list.slideUp("fast");
+				}
+				else {
+				    $(".js-select").removeClass("is-active");
+				    $(".js-select-list").hide();
+				    select_list.slideDown("fast");
+				    $(this).addClass("is-active");
+				}
+				event.stopPropagation();
+			});
+			select_list.find("li").click(function(event) {
+				var id = $(this).attr("data-id");
+				var text = $(this).text();
+				$(this).parent().parent().find(".js-select-text").text(text);
+				$(this).parent().parent().find(".js-select-input").val(id);
+				$(this).parent().hide();
+				$(this).parents(".js-select").removeClass("is-active");
+				event.stopPropagation();
+			});
+		});
+	}
+	select();
+
+	$('.js-select').click(function(event){
+	    event.stopPropagation();
 	});
 
 
@@ -161,7 +198,6 @@ $(document).ready(function() {
 	if($('.gallery__item').length <= 4){
 		$('.js-nextSlider4').find('.caption__btn').remove();
 	};
-
 
 
 	function ui_slider() {
--- build/js/main.js
+++ build/js/main.js
@@ -1825,8 +1825,6 @@ $(document).ready(function() {
 
 
 
-
-
 	/* Скрипт для рейтинга */
 
 
@@ -1876,6 +1874,82 @@ $(document).ready(function() {
 			);
 
 		});
+
+	});
+
+
+
+
+
+	// check select
+
+
+
+	function select() {
+
+		$(".js-select").each(function(){
+
+			var select_list = $(this).parent().find(".js-select-list");
+
+			var text = select_list.find("li").first().text();
+
+			$(this).find(".js-select-text").text(text);
+
+			$(this).click(function(event){
+
+				if ($(this).hasClass("is-active")) {
+
+				    $(this).removeClass("is-active");
+
+				    select_list.slideUp("fast");
+
+				}
+
+				else {
+
+				    $(".js-select").removeClass("is-active");
+
+				    $(".js-select-list").hide();
+
+				    select_list.slideDown("fast");
+
+				    $(this).addClass("is-active");
+
+				}
+
+				event.stopPropagation();
+
+			});
+
+			select_list.find("li").click(function(event) {
+
+				var id = $(this).attr("data-id");
+
+				var text = $(this).text();
+
+				$(this).parent().parent().find(".js-select-text").text(text);
+
+				$(this).parent().parent().find(".js-select-input").val(id);
+
+				$(this).parent().hide();
+
+				$(this).parents(".js-select").removeClass("is-active");
+
+				event.stopPropagation();
+
+			});
+
+		});
+
+	}
+
+	select();
+
+
+
+	$('.js-select').click(function(event){
+
+	    event.stopPropagation();
 
 	});
 
@@ -2000,8 +2074,6 @@ $(document).ready(function() {
 		$('.js-nextSlider4').find('.caption__btn').remove();
 
 	};
-
-
 
 
 
